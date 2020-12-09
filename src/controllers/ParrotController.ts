bf9fbf00d8f0aed5e6efffae7fd4e9a8cfd4d4e4
Roman Zukov <romanzukov1993@gmail.com>
2020-12-09 18:21:00 +0100

    Created controller. Added one missin param to the model. Added tsconfig.json

---
+++ src/controllers/ParrotController.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+const express = require('express');
+const ParrotModel = require('../database/models/Parrot');
+
+class ParrotController {
+    constructor() {
+    }
+
+    public saveParrot = async (req: Request, res: Response) => {
+        try {
+            const newParrot = new ParrotModel(req.body);
+            await newParrot.save();
+            res.status(200).json({status: 200, message: 'Parrot successfully saved'});
+        } catch (error) {
+            res.status(500).json({status: 500, message: 'Error saving parrot', error: error})
+        }
+    }
+
+    public getParrotByUniqueId = async (req: Request, res: Response) => {
+        const uniqueId = req.params.uniqueId;
+        const parrot = await ParrotModel.findOne({uniqueId: uniqueId}).exec();
+
+        if (parrot) {
+            res.status(200).json({status: 200, data: parrot});
+            return;
+        }
+
+        res.status(400).json({status: 400, message: 'Parrot not found!'})
+    }
+
+
+}
